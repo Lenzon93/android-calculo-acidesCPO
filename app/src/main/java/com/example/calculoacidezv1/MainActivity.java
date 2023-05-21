daeaf84af389a08b2acc204e3da1c0b8748242e1
vastolord93 <lensocastillo@gmail.com>
2023-05-21 15:37:24 -0600

    cambio de logo, agrego about.xml, se modifico el formato

--- app/src/main/java/com/example/calculoacidezv1/MainActivity.java
+++ app/src/main/java/com/example/calculoacidezv1/MainActivity.java
@@ -1,15 +1,21 @@
 package com.example.calculoacidezv1;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.widget.Toast;
 import android.os.Bundle;
 import android.view.TextureView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import java.text.DecimalFormat;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final DecimalFormat df = new DecimalFormat("0.00");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,43 +28,60 @@ public class MainActivity extends AppCompatActivity {
         EditText acd2=(findViewById(R.id.entryacd2));
         TextView textoresultado=findViewById(R.id.vResultado);
         Button btnCalculo=findViewById(R.id.btnCalcular);
-        btnCalculo.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                Double tk1D = Double.parseDouble(tk1.getText().toString());
-                Double tk2D = Double.parseDouble(tk2.getText().toString());
-                Double ac1D = Double.parseDouble(acd1.getText().toString());
-                Double ac2D = Double.parseDouble(acd2.getText().toString());
+        Button btnAcercaDe=findViewById(R.id.btnAcercaDe);
 
 
-                Double totalTmTanques,tanque1MasAcidez,tanque2MasAcidez,sumaResultados,resultado;
+           btnCalculo.setOnClickListener(new View.OnClickListener() {
+               @Override
+               public boolean equals(@Nullable Object obj) {
+                   return super.equals(obj);
+               }
 
-                // primer calculo. se suman los don TM de los tanques
+               @Override
 
-                totalTmTanques=tk1D+tk2D;
+               public void onClick(View v) {
 
-                // segundo calculo. es multiplicar la medida de TM del primer tanque con la acidez correspondiente
+                   Float tk1D = Float.parseFloat(tk1.getText().toString());
+                   Float tk2D = Float.parseFloat(tk2.getText().toString());
+                   Float ac1D = Float.parseFloat(acd1.getText().toString());
+                   Float ac2D = Float.parseFloat(acd2.getText().toString());
 
-                tanque1MasAcidez=tk1D*ac1D;
-                tanque2MasAcidez=tk2D*ac2D;
 
-                // tercer calculo. se suman los resultado de del segundo calculo
+                   Float totalTmTanques,tanque1MasAcidez,tanque2MasAcidez,sumaResultados,resultado;
 
-                sumaResultados=tanque1MasAcidez+tanque2MasAcidez;
+                   // primer calculo. se suman los don TM de los tanques
 
-                //Cuarto calculo.Se divide el dato del tercer resultado entre el dato del primer calculo
+                   totalTmTanques=tk1D+tk2D;
 
-                resultado= sumaResultados/totalTmTanques;
+                   // segundo calculo. es multiplicar la medida de TM del primer tanque con la acidez correspondiente
 
-                //muestra por pantalla el resultado del calculo de la acidez final
-                textoresultado.setText("Con un tanque de : "+tk1D+" TM, que tiene una acidez de : "+ac1D+" AGL. " +
-                        "Si le sumamos las "+ tk2D+ " TMS del segundo Tanque que tiene la acidez de "+
-                        ac2D+" AGl . Nos da el resultado de :"+resultado+" AGL");
+                   tanque1MasAcidez=tk1D*ac1D;
+                   tanque2MasAcidez=tk2D*ac2D;
+
+                   // tercer calculo. se suman los resultado de del segundo calculo
+
+                   sumaResultados=tanque1MasAcidez+tanque2MasAcidez;
+
+                   //Cuarto calculo.Se divide el dato del tercer resultado entre el dato del primer calculo
+
+                   resultado= sumaResultados/totalTmTanques;
+
+                   //muestra por pantalla el resultado del calculo de la acidez final
+                   textoresultado.setText("Tm final de la mezcla: "+ totalTmTanques+"Tm. La acidez final es de: "+df.format(resultado)+" AGL");
+
 
 
             }
         });
+
+           btnAcercaDe.setOnClickListener(new View.OnClickListener() {
+               @Override
+               public void onClick(View v) {
+                   setContentView(R.layout.about);
+               }
+           });
+
+
 
     }
 
