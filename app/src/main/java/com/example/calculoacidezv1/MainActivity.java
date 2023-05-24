9680eda372fd2207096daea39572138a0756e541
vastolord93 <lensocastillo@gmail.com>
2023-05-23 23:02:06 -0600

    se manejo la excepcion del boto que hacia que la app se caera

--- app/src/main/java/com/example/calculoacidezv1/MainActivity.java
+++ app/src/main/java/com/example/calculoacidezv1/MainActivity.java
@@ -4,12 +4,13 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import android.widget.Toast;
 import android.os.Bundle;
-import android.view.TextureView;
+
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import java.text.DecimalFormat;
+
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
@@ -31,48 +32,69 @@ public class MainActivity extends AppCompatActivity {
         Button btnAcercaDe=findViewById(R.id.btnAcercaDe);
 
 
-           btnCalculo.setOnClickListener(new View.OnClickListener() {
-               @Override
-               public boolean equals(@Nullable Object obj) {
-                   return super.equals(obj);
-               }
-
-               @Override
-
-               public void onClick(View v) {
-
-                   Float tk1D = Float.parseFloat(tk1.getText().toString());
-                   Float tk2D = Float.parseFloat(tk2.getText().toString());
-                   Float ac1D = Float.parseFloat(acd1.getText().toString());
-                   Float ac2D = Float.parseFloat(acd2.getText().toString());
-
-
-                   Float totalTmTanques,tanque1MasAcidez,tanque2MasAcidez,sumaResultados,resultado;
-
-                   // primer calculo. se suman los don TM de los tanques
-
-                   totalTmTanques=tk1D+tk2D;
-
-                   // segundo calculo. es multiplicar la medida de TM del primer tanque con la acidez correspondiente
-
-                   tanque1MasAcidez=tk1D*ac1D;
-                   tanque2MasAcidez=tk2D*ac2D;
-
-                   // tercer calculo. se suman los resultado de del segundo calculo
-
-                   sumaResultados=tanque1MasAcidez+tanque2MasAcidez;
-
-                   //Cuarto calculo.Se divide el dato del tercer resultado entre el dato del primer calculo
-
-                   resultado= sumaResultados/totalTmTanques;
-
-                   //muestra por pantalla el resultado del calculo de la acidez final
-                   textoresultado.setText("Tm final de la mezcla: "+ totalTmTanques+"Tm. La acidez final es de: "+df.format(resultado)+" AGL");
 
 
 
-            }
-        });
+                    btnCalculo.setOnClickListener(new View.OnClickListener() {
+                        int indicador;
+
+                        float tk1D,tk2D,ac1D,ac2D;
+                        @Override
+                        public boolean equals(@Nullable Object obj) {
+                            return super.equals(obj);
+                        }
+
+                        @Override
+
+                        public void onClick(View v) {
+                            try {
+
+                                 tk1D = Float.parseFloat(tk1.getText().toString());
+                                 tk2D = Float.parseFloat(tk2.getText().toString());
+                                 ac1D = Float.parseFloat(acd1.getText().toString());
+                                 ac2D = Float.parseFloat(acd2.getText().toString());
+                                 indicador=0;
+
+                            }catch (Exception e){
+                                indicador=1;
+                                Toast.makeText(getApplicationContext(),"Favor rellene los espacios en blanco",Toast.LENGTH_LONG).show();
+                            }
+
+                            Float totalTmTanques, tanque1MasAcidez, tanque2MasAcidez, sumaResultados, resultado;
+
+                            // primer calculo. se suman los don TM de los tanques
+
+                            totalTmTanques = tk1D + tk2D;
+
+                            // segundo calculo. es multiplicar la medida de TM del primer tanque con la acidez correspondiente
+
+                            tanque1MasAcidez = tk1D * ac1D;
+                            tanque2MasAcidez = tk2D * ac2D;
+
+                            // tercer calculo. se suman los resultado de del segundo calculo
+
+                            sumaResultados = tanque1MasAcidez + tanque2MasAcidez;
+
+                            //Cuarto calculo.Se divide el dato del tercer resultado entre el dato del primer calculo
+
+                            resultado = sumaResultados / totalTmTanques;
+
+                            //muestra por pantalla el resultado del calculo de la acidez final
+
+                            if(indicador==1){
+                                textoresultado.setText("Ninguna Operacion Realizada");
+
+                            }else if(indicador==0) {
+                                textoresultado.setText("TM final de la mezcla: " + totalTmTanques + "Tm. La acidez final es de: " + df.format(resultado) + " AGL");
+                            }else{
+
+                                textoresultado.setText("Ninguna Operacion Realizada");
+                            }
+
+                        }
+                    });
+
+
 
            btnAcercaDe.setOnClickListener(new View.OnClickListener() {
                @Override
@@ -80,6 +102,8 @@ public class MainActivity extends AppCompatActivity {
                    setContentView(R.layout.about);
                }
            });
+
+
 
 
 
